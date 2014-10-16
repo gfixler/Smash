85212ff36d43089dfdcab63e0d349c6229d8bf2a
gfixler <gary@garyfixler.com>
2014-10-15 18:41:44 -0700

    Begin curve module with 1-degree curve function

---
+++ Curve.hs
@@ -0,0 +1,15 @@
+import Data.List
+
+
+type Point = (Float,Float,Float)
+
+strPt :: Point -> String
+strPt = show
+
+curve1 :: [Point] -> String
+curve1 ps = "cmds.curve(degree=1, points=("
+            ++ (intercalate ", " $ map strPt ps)
+            ++ "), knots=("
+            ++ (intercalate ", " $ map show [0..length ps - 1])
+            ++ "))"
+
